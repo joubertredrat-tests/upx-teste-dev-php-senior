ed167b870871439881dbbacce69e6ec34eaec6dc
Joubert RedRat <me+github@redrat.com.br>
2018-03-25 20:21:16 -0300

    Add tag model and repository

---
+++ src/Task/Model/Tag.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Acme\Task\Model;
+
+/**
+ * Tag
+ *
+ * @package Acme\Task\Model
+ */
+class Tag
+{
+    use DateTimeTrait;
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $textColor;
+
+    /**
+     * @var string
+     */
+    private $backgroundColor;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTextColor(): ?string
+    {
+        return $this->textColor;
+    }
+
+    /**
+     * @param string $textColor
+     */
+    public function setTextColor(string $textColor): void
+    {
+        $this->textColor = $textColor;
+    }
+
+    /**
+     * @return string|null
+     * @return void
+     */
+    public function getBackgroundColor(): ?string
+    {
+        return $this->backgroundColor;
+    }
+
+    /**
+     * @param string $backgroundColor
+     * @return void
+     */
+    public function setBackgroundColor(string $backgroundColor): void
+    {
+        $this->backgroundColor = $backgroundColor;
+    }
+}
